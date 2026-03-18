66b6fca08bfb72fb252fb7e6a90735d2d7d98cc4
Rory Durrant <hi@rory.gg>
2026-03-18 23:00:37 +0000

    feat: add package exports and remove prototype scripts

--- ilidl/__init__.py
+++ ilidl/__init__.py
@@ -1,1 +1,6 @@
 """Unofficial Lidl Plus API client."""
+
+from ilidl.client import LidlClient
+from ilidl.models import Coupon, Discount, Item, Receipt, Store
+
+__all__ = ["LidlClient", "Coupon", "Discount", "Item", "Receipt", "Store"]
