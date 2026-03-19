9a8394f45f130e357e95f03a996a682a9d13a10e
Rory Durrant <hi@rory.gg>
2026-03-19 12:45:20 +0000

    feat: add __version__ to package

--- ilidl/__init__.py
+++ ilidl/__init__.py
@@ -4,3 +4,4 @@ from ilidl.client import LidlClient
 from ilidl.models import Coupon, Discount, Item, Receipt, Store
 
 __all__ = ["LidlClient", "Coupon", "Discount", "Item", "Receipt", "Store"]
+__version__ = "0.1.0"
